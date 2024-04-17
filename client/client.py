15be8cdbdc5dc4673aa88726bc5b6254c0bdd8af
Raiden van Bronkhorst <rvanbronkhorst@gmail.com>
2024-04-17 14:22:14 -0700

    Implement client and server

--- client/client.py
+++ client/client.py
@@ -1,36 +1,99 @@
 from jose import jwt
 import time
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 import requests
+from pathlib import Path
+import sys
+
+import argparse
+import yaml
 
 
-# Load the private key from the PEM file
-with open("private.pem", "rb") as key_file:
-    private_key = key_file.read()
+def create_headers(contrib_id, contrib_email, exp, private_key):
+    payload = {
+        "sub": contrib_id,
+        "email": contrib_email,
+        "iat": int(time.time()),
+        "exp": int(time.time()) + exp
+    }
 
-# User information
-contrib_id = "UNV"
-contrib_email = "user@uni.com"
+    # Create the JWT token using RS256 algorithm
+    token = jwt.encode(payload, private_key, algorithm="RS256")
 
-# Generate the JWT
-payload = {
-    "sub": contrib_id,
-    "email": contrib_email,
-    "iat": int(time.time()),
-    "exp": int(time.time()) + 3600
-}
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
 
-# Create the JWT token using RS256 algorithm
-token = jwt.encode(payload, private_key, algorithm="RS256")
+    return headers
 
-# Make the HTTP POST request with the JWT in the Authorization header
-headers = {
-    "Authorization": f"Bearer {token}"
-}
-data = {"file": "new_records.xml", "stream": "uni1"}
+# Function to decrypt a message
+def decrypt_message(encrypted_message, private_key):
+    return private_key.decrypt(
+        bytes.fromhex(encrypted_message),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
 
-response = requests.post("http://127.0.0.1:5000/api/endpoint", headers=headers, json=data)
+def upload_file(file_path, url):
+    # Upload the file to the presigned URL
+    with open(file_path, "rb") as file:
+        response = requests.put(url, data=file)
+    
+    if response.status_code != 200:
+        print(response.status_code, file=sys.stderr)
+        print(response.text, file=sys.stderr)
+        return
 
-print(response.status_code)
-print(response.text)
+    print("File uploaded successfully")
+
+    
+def request_presigned(filename, config):
+    # Get private key
+    private_key_path = Path(config["key_dir"]) / "private.pem"
+    
+    with open(private_key_path, "rb") as key_file:
+        private_key = key_file.read()
+
+    # Create the headers
+    headers = create_headers(
+        config["contrib_id"],
+        config["contrib_email"],
+        config["exp"],
+        private_key,
+    )
+        
+    # Make the HTTP POST request with the JWT in the Authorization header
+    data = {"file": filename, "stream": "uni1"}
+    response = requests.post("http://127.0.0.1:5000/api/endpoint", headers=headers, json=data)
+ 
+    if response.status_code != 200:
+        print(response.status_code, file=sys.stderr)
+        print(response.text, file=sys.stderr)
+        return
+
+    # Decrypt the URL
+    # encrypted_url = response.text
+    # url = decrypt_message(encrypted_url, private_key).decode("utf-8")
+
+    return response.text
+    
+
+def main():
+    with open("config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", help="File to be uploaded", required=True)
+    args = parser.parse_args()
+
+    url = request_presigned(args.file, config)
+
+    upload_file(args.file, url)
+
+
+if __name__ == "__main__":
+    main()
--- server/server/app.py
+++ server/server/app.py
@@ -1,18 +1,42 @@
 from pathlib import Path
 
-from cryptography.hazmat.primitives import serialization
+import boto3
+import yaml
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 from flask import Flask, request
 from jose import jwt
 
 app = Flask(__name__)
 
-key_dir = Path(__file__).resolve().parent.parent
-public_key_path = key_dir / "public.pem"
 
-with open(public_key_path, "rb") as key_file:
-    app.config['public_key'] = serialization.load_pem_public_key(
-        key_file.read()
+def generate_presigned_url(bucket_name, object_name, expiration):
+    """
+    Generate a pre-signed URL to upload a file to an S3 bucket.
+    """
+    try:
+        response = s3_client.generate_presigned_url(
+            "put_object",
+            Params={"Bucket": bucket_name, "Key": object_name},
+            ExpiresIn=expiration
+        )
+        return response
+    except Exception as e:
+        print(f"Error generating pre-signed URL: {e}")
+        return None
+
+# Function to encrypt a message
+def encrypt_message(message, public_key):
+    return public_key.encrypt(
+        message.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
     )
+
+
 
 @app.route("/api/endpoint", methods=["POST"])
 def handle_request():
@@ -20,17 +44,51 @@ def handle_request():
     if auth_header:
         try:
             token = auth_header.split(" ")[1]
-            jwt_payload = jwt.decode(token, app.config['public_key'], algorithms=["RS256"])
+            jwt_payload = jwt.decode(
+                token, app.config["public_key"], algorithms=["RS256"]
+            )
             # Proceed with the request, payload contains the decoded JWT
-            print(auth_header)
+            #print(auth_header)
             print(jwt_payload)
             # print the post request data
             print(request.json)
-            return "Success", 200
+
+            # Return a presigned url
+            presigned_url = generate_presigned_url(
+                config["bucket_name"], object_name, url_expiration
+            )
+
+            # encrypted_url = encrypt_message(presigned_url, app.config["public_key"])
+            # return encrypted_url.hex(), 200
+
+            return presigned_url, 200
         except jwt.JWTError:
             return "Invalid token", 401
     else:
         return "Missing token", 401
 
+
 if __name__ == "__main__":
+    with open("config.yaml", "r") as config_file:
+        config = yaml.safe_load(config_file)
+
+    # Specify the file name that will appear in the bucket once uploaded
+    object_name = "raiden_test.txt"
+
+    # URL expiration time in seconds
+    url_expiration = 3600  # 1 hour
+
+    # Create a Boto3 session with the specified profile
+    session = boto3.Session(profile_name=config["profile"], region_name="us-west-2")
+
+    # Create an S3 client from the session
+    s3_client = session.client("s3")
+
+    key_dir = Path(config["key_dir"])
+
+    public_key_path = key_dir / "public.pem"
+
+    with open(public_key_path, "rb") as key_file:
+        app.config["public_key"] = serialization.load_pem_public_key(key_file.read())
+
     app.run()
